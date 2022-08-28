933ee6381268ef097cfc30e824761097cac3b409
stef <paulkelele@gmail.com>
2022-08-28 13:10:47 +0200

    c

--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
-	"html/template"
 )
 
 // go tool dist list => pour avoir la liste de toutes les OS/ARCH
 // GOOS=android GOARCH=arm64 go build . => pour lancer le build windows par exemple
-
- 
-
-func main() {
-	
-	 http.HandleFunc("/", HomeHandler)
-	 log.Fatal(http.ListenAndServe(":8000", nil))	  
+// /!\ Les noms des variables dans les structs doivent être en Maj
+//
+//	pour être exportées dans le html....
+type dataIndexHTML struct {
+	Titre string
+	Nom   string
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request){
-	tmpl := template.Must(template.ParseFiles("index.html"))
-	tmpl.Execute(w,nil)
+func main() {
+
+	http.HandleFunc("/", HomeHandler)
+	http.Handle("/html/", http.StripPrefix("/html/", http.FileServer(http.Dir("./public/html"))))
+
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./public/css"))))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./public/images"))))
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("public/html/index.html"))
+	data := dataIndexHTML{
+		Titre: "Mon titre",
+	}
+	tmpl.Execute(w, data)
 }
